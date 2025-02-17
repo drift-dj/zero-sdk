be6b60d63438505f8992f6e509891d13d1cd91c6
Nic Starke <nicolaus.starke@gmail.com>
2025-02-17 21:22:40 +0100

    build: add -c argument to build script, this cleans (deletes) the build directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,23 @@
+# Parse input arguments
+CLEAN_BUILD=false
+while getopts "c" opt; do
+  case ${opt} in
+    c )
+      CLEAN_BUILD=true
+      ;;
+    \? )
+      echo "Usage: cmd [-c]"
+      exit 1
+      ;;
+  esac
+done
+
+# Perform a clean build if -c is specified
+if [ "$CLEAN_BUILD" = true ]; then
+  echo "Cleaning build directory..."
+  rm -rf build
+fi
+
 # Get directory of this script
 DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
